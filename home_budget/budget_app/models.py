8bb93f85bfeb4b22abd7d5a6fd487e8b7a231150
Tomislav Nazifović <tomislav.nazifovic@gmail.com>
2025-07-05 17:43:52 +0200

    Migration for models (Category, Expense, Income)

--- home_budget/budget_app/models.py
+++ home_budget/budget_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    """Income/expense categories. Can take other categories as parents, to create subcategories.
+    Both category and subcategory can be used as item category and as a parent."""
+    class CategoryType(models.TextChoices):
+        INCOME = 'income', 'Income'
+        EXPENSE = 'expense', 'Expense'
+
+    name = models.CharField(max_length=100)
+    type = models.CharField(
+        max_length=7,
+        choices=CategoryType.choices,
+        default=CategoryType.EXPENSE,
+    )
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        related_name='subcategories',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.parent} -> {self.name}" if self.parent else self.name
+
+class Expense(models.Model):
+    """Expense items."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Income(models.Model):
+    """Income items."""
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
--- home_budget/budget_app/views.py
+++ home_budget/budget_app/views.py
@@ -5,3 +5,62 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+
+
+
+# from rest_framework import viewsets, permissions
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
+# from rest_framework.decorators import action
+# from .models import Expense, Income, Category
+# from .serializers import ExpenseSerializer, IncomeSerializer, CategorySerializer
+# from django.db.models import Sum
+
+# class ExpenseViewSet(viewsets.ModelViewSet):
+#     serializer_class = ExpenseSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def get_queryset(self):
+#         return Expense.objects.filter(user=self.request.user)
+
+#     def perform_create(self, serializer):
+#         serializer.save(user=self.request.user)
+
+# class IncomeViewSet(viewsets.ModelViewSet):
+#     serializer_class = IncomeSerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def get_queryset(self):
+#         return Income.objects.filter(user=self.request.user)
+
+#     def perform_create(self, serializer):
+#         serializer.save(user=self.request.user)
+
+# class CategoryViewSet(viewsets.ModelViewSet):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+#     permission_classes = [permissions.IsAuthenticated]
+
+# class CurrentStateView(APIView):
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def get(self, request):
+#         total_income = Income.objects.filter(user=request.user).aggregate(Sum('amount'))['amount__sum'] or 0
+#         total_expense = Expense.objects.filter(user=request.user).aggregate(Sum('amount'))['amount__sum'] or 0
+#         return Response({
+#             'total_income': total_income,
+#             'total_expense': total_expense,
+#             'current_balance': total_income - total_expense
+#         })
+
+# class ExpenseByCategoryView(APIView):
+#     permission_classes = [permissions.IsAuthenticated]
+
+#     def get(self, request):
+#         data = (
+#             Expense.objects.filter(user=request.user)
+#             .values('category__name')
+#             .annotate(total=Sum('amount'))
+#         )
+#         return Response(data)
